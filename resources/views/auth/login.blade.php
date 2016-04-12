58710d3d6247d540146f5e17f57b3188c8e13b2b
Server <server@reniec.gob.pe>
2016-04-12 18:56:03 -0500

    changes login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
             <div class="col s12 m6 l4 offset-m3 offset-l4 container-login">
                 <h1>ECEP - ADMIN</h1>
                 <p>Ingrese con cualquiera de las siguientes opciones:</p>
-                <a href="#" class="login-dnie">Ingrese con DNIe</a>
+                <a href="{{ pHelper::baseUrl('/dnie') }}" class="login-dnie">Ingrese con DNIe</a>
                 <a href="{{ $loginGoogle }}" class="login-google">Ingrese con Google</a>
                 <a href="{{ $loginFacebook }}" class="login-facebook">Ingrese con Facebook</a>
                 <a href="{{ $loginTwitter }}" class="login-twitter">Ingrese con Twitter</a>
